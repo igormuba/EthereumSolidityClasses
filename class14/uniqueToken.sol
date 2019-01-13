51c9a4cda86fd691727906bcf15e7e9f97eb5b8f
igormuba <igorm-uba@hotmail.com>
2019-01-13 18:50:01 -0200

    Update uniqueToken.sol

--- class14/uniqueToken.sol
+++ class14/uniqueToken.sol
@@ -43,6 +43,8 @@ contract uniqueToken{
     }
     
     function withdraw() public{
+        uint toSend = balance[msg.sender];
+	    balance[msg.sender]=0;
         msg.sender.transfer(balance[msg.sender]);
     }
     
