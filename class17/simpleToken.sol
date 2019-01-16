27f6175fabc4db0018e373654e90e42d8cd79588
igormuba <igorm-uba@hotmail.com>
2019-01-16 17:49:42 -0200

    Create simpleToken.sol

---
+++ class17/simpleToken.sol
@@ -0,0 +1,28 @@
+pragma solidity ^0.5.0;
+
+contract simpleToken{
+    mapping(address => uint) public balance;
+    address public icoAddress;
+    
+    constructor(address _icoAddress) public {
+        icoAddress=_icoAddress;
+        balance[_icoAddress]=1000;
+    }
+    
+    function transfer(address _receiver, uint _amount) public {
+        require(balance[msg.sender]>=_amount);
+        require(balance[msg.sender]-_amount>=0);
+        balance[msg.sender]-=_amount;
+        balance[_receiver]+=_amount;
+    }
+    
+    function createToken(address _receiver, uint _amount) public{
+        require(msg.sender == icoAddress);
+        balance[icoAddress]-=_amount;
+        balance[_receiver]+=_amount;
+    }
+    
+    function getMyBalance() public view returns (uint){
+        return balance[msg.sender];
+    }
+}
