7ef388255153482b80aed9f8ffee0b3576290bfd
igormuba <igorm-uba@hotmail.com>
2019-01-24 22:22:11 -0200

    Create APIContract.sol

---
+++ Class23/Demonstration/APIContract.sol
@@ -0,0 +1,10 @@
+pragma solidity ^0.5.0;
+
+contract APIContract{
+    function mint(address whatever) public{
+        
+    }
+    function getBalance(address doesNotMatter) public view returns (uint){
+        
+    }
+}
